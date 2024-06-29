8813aca76f2726b9d30fec54558cc6f0dd30458e
Nurullah Orak <orak6884@gmail.com>
2024-06-29 18:46:27 +0300

    An interface was created in the repository.

---
+++ src/main/java/org/orak/thirthwebapplication/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package org.orak.thirthwebapplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository {
+
+}
--- src/main/java/org/orak/thirthwebapplication/service/UserService.java
+++ src/main/java/org/orak/thirthwebapplication/service/UserService.java
@@ -1,12 +1,16 @@
 package org.orak.thirthwebapplication.service;
 
 import org.orak.thirthwebapplication.entity.User;
+import org.orak.thirthwebapplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 
 public class UserService {
 
+    @Autowired
+    private UserRepository repository;
     public  User getUser(){
         User user =new User();
         user.userId=1;
